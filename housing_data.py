f972e863973fee1b677606d03e1e4384deffa478
jossl95 <jos.slabbekoorn@outlook.com>
2019-08-23 16:20:03 +0200

    --

--- housing_data.py
+++ housing_data.py
@@ -8,3 +8,5 @@ max_page = 288
 for page in range(max_page+1):
     url = f'https://www.boliga.dk/salg/resultater?salesDateMin=1995&zipcodeFrom=1000&zipcodeTo=2499&searchTab=1&page={page}&sort=date-a'
     print(url)
+
+import Connector
