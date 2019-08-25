cbfeeeb5a464c87e6a41a67995b2b54ad7163b40
jossl95 <jos.slabbekoorn@outlook.com>
2019-08-25 16:33:06 +0200

    implementation of selenium scraper to circumvent redirection by boliga.

--- data_scraping.py
+++ data_scraping.py
@@ -137,97 +137,132 @@ from datetime import datetime
 import scraping_class
 import re
 
-##################################
-#      Scraping from Boliga      #
-##################################
-# Downloading relevant packages for geoppy and connector
-from geopy.geocoders import Nominatim
-from geopy.extra.rate_limiter import RateLimiter
-from Connector import Connector, ratelimit
-import time
-import os
+#Initialization of selenium
+path2gecko = '/Users/MacbookJos/git/geckodriver' # define path to your geckodriver
+browser = webdriver.Firefox(executable_path=path2gecko) # start the browser with a path to the geckodriver.
 
-# Using connector to create a log file of our data
-logfile = 'Scraping_file.csv' # Naming our log file
-connector = Connector(logfile)
+# direction to boliga.dk
+browser.get('https://www.boliga.dk/salg/resultater?salesDateMin=1995&zipcodeFrom=1000&zipcodeTo=2499&searchTab=1&page=1&sort=date-a')
+cookie_button = browser.find_element_by_xpath('//*[@id="coiAccept"]')
+cookie_button.click()
 
-# Defining a fucntion which scrapes and process the data, and get longtitude and Altitude
-def scraping_function(url):
-    response, call_id = connector.get(url,'SDS_exam scrape')
-
-    # Checks if our response fucntion is working properly
-    if response.ok:
-        html = response.text
-    else:
-        print('error')
-
-    # Transforming the response into a Pandas dataframe
-    df_table = pd.read_html(response.text)[0]
-
-    # Fixes problem whith column name in each row - replaces column name with whitespace
-    for column in list(df_table.columns):
-        df_table[column] = df_table[column].str.replace(column, '')
-
-    # Tanslating the column names to english, and delete two columns that we don't intend to use
-    df_table.columns = ['Address', "Sell_price", "Date_of_sale", "Type",
-                        "sqm_price", "Rooms", "m2","Building_year", "PD", "UN"]
-    df_table = df_table.drop(['PD', 'UN'], axis = 1 )
+def df2table(html):
+    df = pd.read_html(html)[0]
+    df = df.iloc[:, 0:8]
+    for column in list(df.columns):
+        df[column] = df.loc[column].str.replace(column, '')
+    df.columns = ["Address", "Sell_price", "Date_of_sale", "Type",
+                  "sqm_price", "Rooms", "m2", "Building_year"]
 
     #  Removing dublicates of the type of residence
-    for i in range(len(df_table['Type'])):
+    for i in range(len(df['Type'])):
         for j in ['EEjerlejlighed', 'VVilla', 'RRækkehus']:
-            df_table['Type'][i] = df_table['Type'][i].replace(j,'')
+            df['Type'][i] = df['Type'][i].replace(j,'')
 
     # Changing the prices, so that it's numeric values
-    for s in range(len(df_table['Sell_price'])):
-        df_table['Sell_price'][s] = df_table['Sell_price'][s].replace('.','')
-        df_table['sqm_price'][s] = df_table['sqm_price'][s].replace('.','')
+    for s in range(len(df['Sell_price'])):
+        df['Sell_price'][s] = df['Sell_price'][s].replace('.','')
+        df['sqm_price'][s] = df['sqm_price'][s].replace('.','')
 
-    df_table['sqm_price'] = pd.to_numeric(df_table['sqm_price'])
-    df_table['Sell_price'] = pd.to_numeric(df_table['Sell_price'])
+    df['sqm_price'] = pd.sqm_price.astype('float').dtypes
+    df['Sell_price'] = pd.Self_price.astype('float').dtypes
+    df['Date_of_sale'] = pd.to_datetime(df.Date_of_sale, format='%d-%m-%Y')
 
-    # Changing the date into date-time format
-    def format_dates(date):
-        q = date.split('-')
-        return q[1].strip() + q[0].strip() + q[2]
+    return df
 
-    df_table['Date_of_sale'] = df_table['Date_of_sale'].apply(lambda x: format_dates(x))
-    df_table['Date_of_sale'] = pd.to_datetime(df_table['Date_of_sale'], format='%m%d%Y')
-
-    # Saves the file path
-    dir = str(os.getcwd())
-    file_path = str(dir + '/boliga' +'/scrape' +'/%d'%call_id)
-
-    with open(file_path, mode='w', encoding='UTF-8',
-              errors='strict', buffering=1) as f:
-        f.write(df_table.to_csv())
-
-    return df_table
-
-# Making folders for the exam project
+i = 1
 dir = str(os.getcwd())
-project = 'boliga'
+while i <= 10:
+    ## Initialization of log
+    logfile = 'log_boliga_csv'
+    project_name = 'SDS exam'
+    header = ['id','project','connector_type','t', 'delta_t', 'url',\
+              'redirect_url','response_size', 'response_code','success',                      'error']
 
-# Create project folder
-if not os.path.isdir(project):    # Checks if the folder exist
-    os.mkdir(project)
+    if os.path.isfile(logfile):
+        log = open(logfile,'a')
+    else:
+        log = open(logfile,'w')
+        log.write(';'.join(header))
 
-# Making a for loop, which loops through page 1 to 288, so that we get data from  01/01/1995 - 31/12/2007
-price_data = []
-max_page = 15 #288
-for p in range(10, 10+max_page):
-    url = 'https://www.boliga.dk/salg/resultater?salesDateMin=1995&zipcodeFrom=1000&zipcodeTo=2499&searchTab=1&page='+'%d&sort=date-a'%p
-    price_data.append(scraping_function(url))
-    time.sleep(4)
+    ## load log
+    with open(logfile,'r') as f: # open file
+        l = f.read().split('\n') # read and split file by newlines.
+        ## set id
+        if len(l)<=1:
+            id = 0
+        else:
+            id = int(l[-1][0])+1
 
-# Convert the list into one table and resets index
-boliga_data = pd.concat(price_data, axis=0)
-boliga_data = boliga_data.reset_index()
+    t = time.time()
+    try: #defines error handling
+        """
+        NOTE: several indicators are different than from the indicators
+        that are established with the requests package. `dt`, does not
+        necessarily reflect the complete load time. `size` might not  be
+        correct as selenium works in the background and could still be
+        loading
+        """
+        html = browser.page_source
 
-# Saves the data in a csv file
-file_path = dir + "/boliga/data/housing_data.csv"
+        ## Key arguments
+        err = ''                           # define python error variable as empty assumming success.
+        success = True                     # define success variable
+        connector_type = "selenium"
+        redirect_url = browser.current_url # log current url, after potential redirects
+        dt = t - time.time()               # define delta-time waiting for the server and downloading content.
+        size = len(browser.page_source)    # define variable for size of html content of the response.
+        response_code = ''                 # log status code.
+        ## log...
+        call_id = i                        # get current unique identifier for the call
+
+        # Defines row to be written in the log
+        row = [call_id, project_name, connector_type, t, dt,\
+               redirect_url, size, response_code, success, err] # define row
+
+        log.write('\n'+';'.join(map(str,row))) # write row to log file.
+        log.flush()
+
+        # storing parsed table as csv
+        df = df2table(html)
+        file_path = str(dir + '/boliga' +'/scrape' +'/%d.csv'%i)
+        with open(file_path, mode='w', encoding='UTF-8',
+                  errors='strict', buffering=1) as f:
+            f.write(df.to_csv())
+        time.sleep(2)
+
+    except Exception as e:          # define error condition
+        log.flush()
+
+    xpath_next = '/html/body/app-root/app-sold-properties-list/div[2]/app-pagination/div[1]/div[4]/a'
+    next_button = browser.find_element_by_xpath(xpath_next)
+    next_button.click()
+    time.sleep(1)
+    i += 1
+
+##############################
+#    Apppending csv-files    #
+##############################
+
+#Path to file directory
+boliga_directory = dir + '/boliga/scrape/'
+
+#import all the files in the folder
+boliga_files = glob.glob(os.path.join(boliga_directory, '*.csv'))
+
+#loop throug all files and read as pandas
+merged_df = [] #saves as list of dataframes
+for boliga_file in boliga_files:
+    df = pd.read_csv(boliga_file)
+    merged_df.append(df)
+
+#merge the dataframes with concat
+boliga_data = pd.concat(merged_df, ignore_index=True)
+
+#save as csv
+file_path = str(dir + '/boliga/data/housing_data.csv')
 with open(file_path, mode='w', encoding='UTF-8',
-              errors='strict', buffering=1) as f:
+          errors='strict', buffering=1) as f:
     f.write(boliga_data.to_csv())
 
 ####################################################
@@ -486,7 +521,7 @@ df_location = pd.DataFrame([street, locations, latitude, longitude],
                      'Longitude']).T
 
 boliga_data = pd.concat([boliga_data, df_location], axis=1, join='inner')
-	
+
 #####################################################
 #                                                   #
 #                 Monthly data                      #
--- scrapy.py
+++ scrapy.py
@@ -1,26 +1,30 @@
+from selenium import webdriver
 import time
 import os
 import pandas as pd
 import Connector
+import glob
+
+#######################
+#    Scraping data    #
+#######################
 
 #Initialization of selenium
 path2gecko = '/Users/MacbookJos/git/geckodriver' # define path to your geckodriver
-browser = Connector('log_boliga.csv',
-                    connector_type= 'selenium',
-                    path2selenium='/Users/MacbookJos/git/geckodriver')
+browser = webdriver.Firefox(executable_path=path2gecko) # start the browser with a path to the geckodriver.
 
 # direction to boliga.dk
-browser.get('https://www.boliga.dk/salg/resultater?salesDateMin=1995&zipcodeFrom=1000&zipcodeTo=2499&searchTab=1&page=1&sort=date-a', "SDS student")
+browser.get('https://www.boliga.dk/salg/resultater?salesDateMin=1995&zipcodeFrom=1000&zipcodeTo=2499&searchTab=1&page=1&sort=date-a')
 cookie_button = browser.find_element_by_xpath('//*[@id="coiAccept"]')
 cookie_button.click()
 
-def html2table(html):
+def df2table(html):
     df = pd.read_html(html)[0]
     df = df.iloc[:, 0:8]
     for column in list(df.columns):
-        df[column] = df[column].str.replace(column, '')
-    df.columns = ['Address', "Sell_price", "Date_of_sale", "Type",
-                  "sqm_price", "Rooms", "m2","Building_year"]
+        df[column] = df.loc[column].str.replace(column, '')
+    df.columns = ["Address", "Sell_price", "Date_of_sale", "Type",
+                  "sqm_price", "Rooms", "m2", "Building_year"]
 
     #  Removing dublicates of the type of residence
     for i in range(len(df['Type'])):
@@ -32,31 +36,103 @@ def html2table(html):
         df['Sell_price'][s] = df['Sell_price'][s].replace('.','')
         df['sqm_price'][s] = df['sqm_price'][s].replace('.','')
 
-    df['sqm_price'] = pd.to_numeric(df['sqm_price'])
-    df['Sell_price'] = pd.to_numeric(df['Sell_price'])
-
-    # Changing the date into date-time format
-    def format_dates(date):
-        q = date.split('-')
-        return q[1].strip() + q[0].strip() + q[2]
-
-    df['Date_of_sale'] = df['Date_of_sale'].apply(lambda x: format_dates(x))
-    df['Date_of_sale'] = pd.to_datetime(df['Date_of_sale'], format='%m%d%Y')
+    df['sqm_price'] = pd.sqm_price.astype('float').dtypes
+    df['Sell_price'] = pd.Self_price.astype('float').dtypes
+    df['Date_of_sale'] = pd.to_datetime(df.Date_of_sale, format='%d-%m-%Y')
 
     return df
 
 i = 1
 dir = str(os.getcwd())
-while i <= 1:
-    df = html2table(html = browser.page_source)
-    file_path = str(dir + '/boliga' +'/scrape' +'/%d.csv'%i)
-    with open(file_path, mode='w', encoding='UTF-8',
-              errors='strict', buffering=1) as f:
-        f.write(df.to_csv())
-    time.sleep(2)
+while i <= 10:
+    ## Initialization of log
+    logfile = 'log_boliga_csv'
+    project_name = 'SDS exam'
+    header = ['id','project','connector_type','t', 'delta_t', 'url',\
+              'redirect_url','response_size', 'response_code','success',                      'error']
 
-    xpath_next = ' /html/body/app-root/app-sold-properties-list/div[2]/app-pagination/div[1]/div[4]/a'
+    if os.path.isfile(logfile):
+        log = open(logfile,'a')
+    else:
+        log = open(logfile,'w')
+        log.write(';'.join(header))
+
+    ## load log
+    with open(logfile,'r') as f: # open file
+        l = f.read().split('\n') # read and split file by newlines.
+        ## set id
+        if len(l)<=1:
+            id = 0
+        else:
+            id = int(l[-1][0])+1
+
+    t = time.time()
+    try: #defines error handling
+        """
+        NOTE: several indicators are different than from the indicators
+        that are established with the requests package. `dt`, does not
+        necessarily reflect the complete load time. `size` might not  be
+        correct as selenium works in the background and could still be
+        loading
+        """
+        html = browser.page_source
+
+        ## Key arguments
+        err = ''                           # define python error variable as empty assumming success.
+        success = True                     # define success variable
+        connector_type = "selenium"
+        redirect_url = browser.current_url # log current url, after potential redirects
+        dt = t - time.time()               # define delta-time waiting for the server and downloading content.
+        size = len(browser.page_source)    # define variable for size of html content of the response.
+        response_code = ''                 # log status code.
+        ## log...
+        call_id = i                        # get current unique identifier for the call
+
+        # Defines row to be written in the log
+        row = [call_id, project_name, connector_type, t, dt,\
+               redirect_url, size, response_code, success, err] # define row
+
+        log.write('\n'+';'.join(map(str,row))) # write row to log file.
+        log.flush()
+
+        # storing parsed table as csv
+        df = df2table(html)
+        file_path = str(dir + '/boliga' +'/scrape' +'/%d.csv'%i)
+        with open(file_path, mode='w', encoding='UTF-8',
+                  errors='strict', buffering=1) as f:
+            f.write(df.to_csv())
+        time.sleep(2)
+
+    except Exception as e:          # define error condition
+        log.flush()
+
+    xpath_next = '/html/body/app-root/app-sold-properties-list/div[2]/app-pagination/div[1]/div[4]/a'
     next_button = browser.find_element_by_xpath(xpath_next)
     next_button.click()
     time.sleep(1)
     i += 1
+
+##############################
+#    Apppending csv-files    #
+##############################
+
+#Path to file directory
+boliga_directory = dir + '/boliga/scrape/'
+
+#import all the files in the folder
+boliga_files = glob.glob(os.path.join(boliga_directory, '*.csv'))
+
+#loop throug all files and read as pandas
+merged_df = [] #saves as list of dataframes
+for boliga_file in boliga_files:
+    df = pd.read_csv(boliga_file)
+    merged_df.append(df)
+
+#merge the dataframes with concat
+boliga_data = pd.concat(merged_df, ignore_index=True)
+
+#save as csv
+file_path = str(dir + '/boliga/data/housing_data.csv')
+with open(file_path, mode='w', encoding='UTF-8',
+          errors='strict', buffering=1) as f:
+    f.write(boliga_data.to_csv())
